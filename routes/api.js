10b1401ea4acb65729c0b0507b9e5023e1f49455
Tezcatlipoca0000 <tezcatlipoca0000@gmail.com>
2023-01-26 22:24:17 -0600

    Begun solving route logic

--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,14 @@ module.exports = function (app) {
 
   app.route('/api/stock-prices')
     .get(function (req, res){
-      
+      console.log('the queries >>> ', req.query);
+      let stockSymbol = req.query.stock;
+      let liked = req.query.like;
+      console.log('the stock symbol >>>> ', stockSymbol);
+      console.log('liked?? ', liked);
+      console.log('typeof stockSymbol', typeof stockSymbol);
+      if (typeof stockSymbol === 'object') var [a, b] = stockSymbol;
+      console.log('destructured symbols ', a, b);
     });
     
 };
