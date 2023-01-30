11c10b3c9fe28f10922c1993c54bc84c4edf1fc4
Tezcatlipoca0000 <tezcatlipoca0000@gmail.com>
2023-01-30 15:02:01 -0600

    Finished the challenge

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -75,7 +75,15 @@ suite('Functional Tests', function() {
             .get('/api/stock-prices/')
             .query({stock: ['GOOG', 'MSFT'], like: 'true'})
             .end(function(err, res) {
-                
+                assert.equal(res.status, 200, 'Response status should be 200');
+                assert.isObject(res.body, 'Response body should be an object');
+                assert.isArray(res.body.stockData, '"stockData" should be an Array');
+                assert.lengthOf(res.body.stockData, 2, '"stockData" should have length of 2');
+                assert.propertyVal(res.body.stockData[0], 'stock', 'GOOG', 'First object in "stockData" should include {stock: "GOOG"}');
+                assert.propertyVal(res.body.stockData[1], 'stock', 'MSFT', 'Second object in "stockData" should include {stock: "MSFT"}');
+                assert.hasAllKeys(res.body.stockData[0], ['stock', 'price', 'rel_likes'], 'First object in "stockData" should have the keys "stock" "price" and "rel_likes"');
+                assert.hasAllKeys(res.body.stockData[1], ['stock', 'price', 'rel_likes'], 'Second object in "stockData" should have the keys "stock" "price" and "rel_likes"');
+                done();
             });
     });
 });
